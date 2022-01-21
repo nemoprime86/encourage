1e57e8776fba9d44aea7b107cf56266980ef9fc4
Steven de Lange <stevenmater@gmail.com>
2022-01-21 12:16:17 +0100

    fixed a spelling error

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 console.log(
-  'Study hard and make sure you understand what and why code does what is does before moving on. You can do this!!!'
+  'Study hard and make sure you understand what and why code does what it does before moving on. You can do this!!!'
 );
